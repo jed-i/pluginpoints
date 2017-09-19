7a73bd2d925333e04b8f5182ae6bbc495afdd773
REPLGROUP\Simon.Bloomer <simon.bloomer@replgroup.com>
2017-09-19 13:32:33 +0100

    mistake commit 1

--- Startup.cs
+++ Startup.cs
@@ -25,6 +25,7 @@ namespace pluginpoints
             services.AddMvc();
         }
 
+		// new feature
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
