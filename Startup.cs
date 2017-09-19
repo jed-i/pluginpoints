b37605a5bd2c3b46a1fd1427cf7a28ab210bc06a
REPLGROUP\Simon.Bloomer <simon.bloomer@replgroup.com>
2017-09-19 14:24:42 +0100

    new changes

--- Startup.cs
+++ Startup.cs
@@ -27,6 +27,8 @@ namespace pluginpoints
 
 		// new feature
 		// feature 2
+		// feature 3
+		// feature 4
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
